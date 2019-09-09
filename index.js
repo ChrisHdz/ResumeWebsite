9a79964d628be30d0c2df04728efcb5da3b7d719
Christian <chrstian.hernandez19@upr.edu>
2019-09-09 11:45:41 -0400

    Adding landing page and itroduction

---
+++ index.js
@@ -0,0 +1,13 @@
+$(function(){
+
+    $('.heading').hide();
+    $('.about').hide();
+    $('.projects').hide();
+    $('.social').hide();
+
+    $('.heading').delay(7000).fadeIn(3000);
+    $('.about').delay(8000).fadeIn(3000);
+    $('.projects').delay(9000).fadeIn(3000);
+    $('.social').delay(10000).fadeIn(3000);
+    $('#type').delay(6200).fadeOut('slow'); //Typewriter animation fadeout delay
+  });
