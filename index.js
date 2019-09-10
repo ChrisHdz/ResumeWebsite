73fdc8489d36a10742b5e92598ae007b3d1f1d99
Christian <chrstian.hernandez19@upr.edu>
2019-09-10 10:09:24 -0400

    Adding introduction hover animation and logo to tittle

--- index.js
+++ index.js
@@ -4,6 +4,30 @@ $(function(){
     $('.about').hide();
     $('.projects').hide();
     $('.social').hide();
+    $('#introAbout').hide();
+    $('#introProjects').hide();
+    $('#introSocail').hide();
+
+
+    $('.about').hover(function() {
+      $('#introAbout').show();
+    }, function() {
+      $('#introAbout').hide();
+    });
+
+    $('.projects').hover(function() {
+      $('#introProjects').show();
+    }, function() {
+      $('#introProjects').hide();
+    });
+
+    $('.social').hover(function() {
+      $('#introSocail').show();
+    }, function() {
+      $('#introSocail').hide();
+    });
+
+
 
     $('.heading').delay(7000).fadeIn(3000);
     $('.about').delay(8000).fadeIn(3000);
